da464120c6b7ba3ac612c0c22088eab5edab1f29
GaneshArjun2004 <arjun2020.2004@gmail.com>
2026-02-12 12:40:17 +0530

    progress2

---
+++ DAY22/mysqltc001.py
@@ -0,0 +1,18 @@
+import mysql.connector
+host="localhost"
+user="root"
+password="database"
+database="feb2026"
+
+conn=mysql.connector.connect(host=host,user=user,password=password,database=database)
+cursor=conn.cursor()
+print("connected to the database successfully")
+
+query="SELECT * FROM employee"
+cursor.execute(query)
+
+result=cursor.fetchall()
+
+for row in result:
+    print(row)
+ 
